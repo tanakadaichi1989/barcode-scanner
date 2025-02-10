e587b55ea2fe8f352249abc3baebc9c54fa65b92
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2025-02-10 23:16:50 +0900

    Edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 import ScanbotSDK from "scanbot-web-sdk/ui";
 
-const App = () => {
+/**
+ * バーコードリーダーの実装方法 下記 URL 参照
+ * https://scanbot.io/techblog/react-barcode-reader-tutorial/
+ */
+
+function App() {
+  const [count, setCount] = useState(0)
   const [scanResult, setScanResult] = useState<string>("");
-  
+
   useEffect(() => {
-    const init = async () => {
+    async function init(): Promise<void> {
       await ScanbotSDK.initialize({
         licenseKey: "",
         enginePath: "/wasm/"
       });
     };
-
     init();
-  }, []);
-  
-  const startScanner = async () => {
+  },[]);
+
+  async function startScanner(){
     const config = new ScanbotSDK.UI.Config.BarcodeScannerScreenConfiguration();
-          
     const result = await ScanbotSDK.UI.createBarcodeScanner(config);
-        
-    if (result && result.items.length > 0) {
+
+    if(result && result.items.length > 0){
       setScanResult(result.items[0].barcode.text);
     }
-    
+
     return result;
   }
 
   return (
-    <div>
-      <h1>Barcode Reader</h1>
-      <button onClick={startScanner}>Start Scanner</button>
-      {scanResult && <div>{scanResult}</div>}
-    </div>
-  );
-};
+    <>
+      <h1>Barcode Scanner</h1>
+      <div className="card">
+        <button onClick={startScanner}>
+          Start Scanner
+        </button>
+        <div>
+          {scanResult && <h2>{scanResult}</h2>}
+        </div>
+      </div>
+    </>
+  )
+}
 
-export default App;
+export default App
