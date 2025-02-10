7c1fb98c26bcadcd2abaad769181dcd15362d944
tanakadaichi_1989 <tanakadaichi_1989@icloud.com>
2025-02-10 22:28:30 +0900

    Implement barcode reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import ScanbotSDK from "scanbot-web-sdk/ui";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [scanResult, setScanResult] = useState<string>("");
+  
+  useEffect(() => {
+    const init = async () => {
+      await ScanbotSDK.initialize({
+        licenseKey: "",
+        enginePath: "/wasm/"
+      });
+    };
+
+    init();
+  }, []);
+  
+  const startScanner = async () => {
+    const config = new ScanbotSDK.UI.Config.BarcodeScannerScreenConfiguration();
+          
+    const result = await ScanbotSDK.UI.createBarcodeScanner(config);
+        
+    if (result && result.items.length > 0) {
+      setScanResult(result.items[0].barcode.text);
+    }
+    
+    return result;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={startScanner}>Start Scanner</button>
+      {scanResult && <div>{scanResult}</div>}
+    </div>
+  );
+};
 
-export default App
+export default App;
